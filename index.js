c62947f8afd150e73f528d30e153c59f24b73805
aldaydev <aldaydev@gmail.com>
2025-12-14 19:31:29 +0100

    countdown done

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const cards = Array.from(document.getElementsByClassName('firstSection__card'));
+import './javascript/cards-observer.js';
+import './javascript/countdown.js';
 
-console.log(cards);
+// const cards = Array.from(document.getElementsByClassName('firstSection__card'));
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('visible');
-      observer.unobserve(entry.target);
-    }
-  })
-}, { threshold: 0.6});
+// console.log(cards);
 
-cards.forEach(card => observer.observe(card));
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add('visible');
+//       observer.unobserve(entry.target);
+//     }
+//   })
+// }, { threshold: 0.6});
+
+// cards.forEach(card => observer.observe(card));
 
---
+++ javascript/countdown.js
@@ -0,0 +1,46 @@
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+const countdownContainerEl = document.getElementById('countdown-container');
+let minutesValue = parseInt(minutesEl.innerText);
+let secondsValue = parseInt(secondsEl.innerText);
+
+console.log(minutesEl, secondsEl);
+
+let interval = null;
+
+function startCountdown() {
+  console.log(minutesValue, secondsValue);
+  interval = setInterval(() => {
+    if(secondsValue === 0) {
+      if(minutesValue !== 0) secondsValue = 60;
+      secondsEl.innerText = secondsValue;
+      if(minutesValue === 0) return stopCountdown();
+      minutesValue--;
+      minutesEl.innerText = formated(minutesValue);
+    }
+    secondsValue--;
+    secondsEl.innerText = formated(secondsValue);
+  }, 1000);
+};
+
+startCountdown();
+
+function stopCountdown() {
+  console.log(countdownContainerEl);
+  clearInterval(interval);
+  interval = null;
+  secondsValue = formated(0);
+  secondsEl.innerText = secondsValue;
+  minutesValue = formated(0);
+  countdownContainerEl.classList.add('stopped');
+};
+
+
+
+function formated(number) {
+  let format2 = new Intl.NumberFormat('en-US', {
+    minimumIntegerDigits: 2,
+    useGrouping: false
+  });
+  return format2.format(number);
+}
