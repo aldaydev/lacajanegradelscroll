4805a1215a81b1f49fb18fedfe670dfc3e58b913
aldaydev <aldaydev@gmail.com>
2025-12-19 03:08:01 +0100

    reviewing current styles + new text content

--- javascript/cards-observer.js
+++ javascript/cards-observer.js
@@ -1,14 +1,35 @@
 const cards = Array.from(document.getElementsByClassName('firstSection__card'));
+const stepLines = Array.from(document.getElementsByClassName('step__line'));
 
 // console.log(cards);
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('visible');
-      observer.unobserve(entry.target);
-    }
-  })
-}, { threshold: 0.6});
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add('visible');
+//       observer.unobserve(entry.target);
+//     }
+//   })
+// }, { threshold: 0.6});
 
-cards.forEach(card => observer.observe(card));
+// cards.forEach(card => observer.observe(card));
+// stepLines.forEach(stepLine => observer.observe(stepLine));
+
+createObserver(cards);
+createObserver(stepLines, 0.2);
+
+function createObserver(elementsArr, threshold = 0.6){
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target);
+      }
+    })
+  }, { threshold: threshold});
+
+  elementsArr.forEach(element => observer.observe(element));
+}
+
+
